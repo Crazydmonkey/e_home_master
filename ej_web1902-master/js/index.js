6aa9cd51cf44c50ac36b3cee52ea6fd9cdd7d277
Crazydmonkey <2627973337@qq.com>
2019-09-24 16:52:32 +0800

    ej原生js的项目代码

--- ej_web1902-master/js/index.js
+++ ej_web1902-master/js/index.js
@@ -1,15 +1,23 @@
 $(function(){
-	// 为栏目绑定事件
-	$("ul.left_nav").on("click","li",function(){
-		// this = event.currentTarget]
-		$("ul.left_nav > li").removeClass("current");
-		$(this).addClass("current");
-		var url = $(this).attr("url");
-		$("#wrapper").load(url)
+	$('.wrapper').load('./pages/customer.html');
+	$('.left ul li').click(function(){
+		$(this).addClass('current');
+		$(this).siblings().removeClass('current');
+		if($(this).text() == '顾客管理'){
+			$('.wrapper').load('./pages/customer.html');
+		}else if($(this).text() == '员工管理'){
+			$('.wrapper').load('./pages/waiter.html');
+		}else if($(this).text() == '栏目管理'){
+			$('.wrapper').load('./pages/category.html');
+		}else if($(this).text() == '地址管理'){
+			$('.wrapper').load('./pages/address.html');
+		}else if($(this).text() == '评论管理'){
+			$('.wrapper').load('./pages/comment.html');
+		}else if($(this).text() == '产品管理'){
+			$('.wrapper').load('./pages/product.html');
+		}
 	})
-
-	// 默认选中第一个li
-	$("ul.left_nav > li:first-child").trigger("click");
-
-
-});
+	$('#index').click(function(){
+		$('.wrapper').load('./pages/highcharts.html');
+	})
+})
